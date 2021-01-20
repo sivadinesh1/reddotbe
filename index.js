21aea57771ed87769af494db576e296cc5a1de3c
Sivadinesh M <sivadinesh@gmail.com>
2021-01-20 14:47:51 +0530

    latest

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const http = require("http");
 const cors = require("cors");
 const fs = require("fs");
 
-// var options = {
-// 	key: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/privkey.pem"),
-// 	cert: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/cert.pem"),
-// 	ca: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/chain.pem"),
-// };
+var options = {
+	key: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/cert.pem"),
+	ca: fs.readFileSync("/etc/letsencrypt/live/demo.squapl.com/chain.pem"),
+};
 
 const app = express();
 
@@ -89,5 +89,5 @@ app.use((err, req, res) => {
 const PORT = process.env.PORT || 5050;
 
 // app.listen(PORT);
-http.createServer(app).listen(5050);
-// https.createServer(options, app).listen(8440);
+//http.createServer(app).listen(5050);
+https.createServer(options, app).listen(8440);
