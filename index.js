59489c8fe9392ac04a5c857cd5bd1c3f35bc1731
Sivadinesh M <sivadinesh@gmail.com>
2021-05-04 22:58:34 +0530

    lates

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.use((err, req, res) => {
 const PORT = process.env.PORT || 5050;
 
 // app.listen(PORT);
-//devlopment env
+//devlopment en
 // http.createServer(app).listen(5050);
 //demo
 // https.createServer(options, app).listen(8440);
